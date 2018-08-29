6043942dc1a2f0ff1571a8f0ec2c2d902bd64d0b
rafael miranda <r4free@gmail.com>
2018-08-29 15:26:47 -0300

    inicio lint

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-import l from "lodash";
-
-class Person {
-  falar(txt) {
-    if (this.isNotMute) {
-      alert(txt);
-    }
-  }
-}
-
-let b = new Person();
-let cre = "asdfasd";
-console.log("ascvcvbvbvd");
-
 for (var i = 0; i < 10; i++) {
   alert(i);
 }
