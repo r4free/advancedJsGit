fa5bfbebd85d44ead81f6edd4d8d5e2c4e465b1b
rafael miranda <r4free@gmail.com>
2018-08-29 14:50:31 -0300

    inicio 2

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ import l from "lodash";
 
 class Person {
   falar(txt) {
-    console.log(txt);
+    if (this.isNotMute) {
+      alert(txt);
+    }
   }
 }
+
+let b = new Person();
 let cre = "asdfasd";
 console.log("ascvcvbvbvd");
+
+for (var i = 0; i < 10; i++) {
+  alert(i);
+}
